738f782596c0b103e65091c1194d9f516fc29c45
nustar <tlstjd436@naver.com>
2021-05-19 12:15:40 +0900

    #210519 - Python 기초 문법 #1 jungol 숙제 (512, 516, 520, 112)

---
+++ Python/day_hw.py
@@ -0,0 +1,33 @@
+# 512
+# a = input()
+# a = float(a)
+# print("%.6f" %(a) )
+
+# 516 : 입력 - 자가진단8
+# a = float(input())
+# b = float(input())
+# c = input()
+# print("%.2f" %(a))
+# print("%.2f" %(b))
+# print(c)
+
+# 520 : 연산자 - 자가진단3
+# a = int(input())
+# print(a)
+# a+=1 # a++
+# a+=1 # ++a
+# print(a)
+
+# 112 : 연산자 - 형성평가2
+a, b= input().split()
+a = int(a)
+b = int(b)
+mok = int(a/b)
+namugi = a%b
+print("%d / %d = %d...%d" %(a,b,mok,namugi))
+
+
+
+# a,b,c = input().split()
+# print(a)
+# print(type(a))
