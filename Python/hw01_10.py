a7d2402dc93d99d8b17f76e56c29a5a21effc314
nustar <tlstjd436@naver.com>
2021-05-23 14:24:54 +0900

    Python 300제 - 1~10문제

---
+++ Python/hw01_10.py
@@ -0,0 +1,31 @@
+#01.
+# print("Hello World")
+
+#02. print 기초
+# 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
+# print("Mary's cosmetics")
+
+#03. 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)
+# 신씨가 소리질렀다. "도둑이야".
+# print('신씨가 소리질렀다."도둑이야".')
+
+#04. 화면에 "C:\Windows"를 출력하세요.
+# print('"C:\Windows"')
+
+#05. 안녕하세요.
+# 만나서    반갑습니다. \n \t
+# print("안녕하세요.\n만나서\t반갑습니다.")
+
+# 07 print 기초
+# print() 함수를 사용하여 다음과 같이 출력하세요.
+# naver;kakao;sk;samsung
+# print("naver", "kakao", "sk", "samsung", sep=";")
+
+# 08 print 기초
+# print() 함수를 사용하여 다음과 같이 출력하세요.
+# naver/kakao/sk/samsung
+# print("naver", "kakao", "sk", "samsung", sep="/")
+
+# 10 나누기 연산자
+# 5/3
+print(5/3)
